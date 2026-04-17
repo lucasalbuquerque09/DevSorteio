10b4dfed3de1d05fda4d1b5b7be08118f3b888cf
lucasalbuquerque09 <lc.albuquerque09@gmail.com>
2026-04-17 20:11:11 -0300

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+
+/* pegar elementos do DOM */
+const botaoSortear = document.getElementById('btn-sortear');
+const resultado = document.querySelector('.result');
+const showResult = document.querySelector('.show-result')
+const hidenResult = document.querySelector('.close')
+
+/* mostrar resultado */
+function mostrarResultado(){
+    resultado.classList.add('show')
+}
+/* esconder resultado */
+hidenResult.addEventListener('click' , ()=>{
+    resultado.classList.remove('show')
+})
+/* sortear número */
+botaoSortear.addEventListener('click' , ()=>{
+    
+    const valorX = Math.ceil( document.getElementById('valor-x').value);
+    const valorY = Math.floor( document.getElementById('valor-y').value); 
+    
+    const result = Math.floor(Math.random() * (valorY - valorX + 1)) + valorX;
+   
+    showResult.textContent = `${result}` 
+
+    mostrarResultado()
+})
+
